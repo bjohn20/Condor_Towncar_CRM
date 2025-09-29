e7d8f7019bee077b8f55ad3b2f78dbac83e494a1
Brandon Johnson <johnson.c.brandon@gmail.com>
2025-09-29 17:51:47 -0400

    Fix: Corrected display destination address on dashboard and time format

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='home'),
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     
